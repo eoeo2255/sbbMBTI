a16c4cc8dc2bd49eb3da6c4a717716e9f8848ea1
eoeo2255 <naiga6@gmail.com>
2023-05-14 15:34:48 +0900

    세팅

---
+++ src/main/java/com/ll/mbti/MainController.java
@@ -0,0 +1,15 @@
+package com.ll.mbti;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @ResponseBody
+    @GetMapping("/mbti")    //  url 과 매핑된 함수는 결과값을 리턴해야 한다.
+    public String index() {
+        return "What's your MBTI?";
+    }
+
+}
