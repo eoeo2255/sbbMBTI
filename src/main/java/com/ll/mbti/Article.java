c6c6523602a61713b4e80793c28122fd462fd278
eoeo2255 <naiga6@gmail.com>
2023-05-14 16:15:50 +0900

    DB 연결, 엔티티 생성

---
+++ src/main/java/com/ll/mbti/Article.java
@@ -0,0 +1,24 @@
+package com.ll.mbti;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter     //  일반적으로 엔티티에는 @Setter 가 추천되지 않는다. 데이터가 자유롭게 변경될 수 있기 때문에 @Builder 를 사용한다.
+@Entity     // 엔티티는 @Column 을 쓰지 않아도 테이블 컬럼으로 인식하기 때문에, 컬럼으로 사용하지 않으려면 @Transient 애너테이션 사용
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(length = 200)
+    private String title;
+    @Column(columnDefinition = "TEXT")      //  columnDefinition = "TEXT" 속성으로 해당 컬럼의  글자수를 제한할 수 없다.
+    private String content;
+    private LocalDateTime createDate;
+    @OneToMany(mappedBy = "comment", cascade = CascadeType.REMOVE)
+    private List<Comment> comments;
+}
---
+++ src/main/java/com/ll/mbti/Comment.java
@@ -0,0 +1,22 @@
+package com.ll.mbti;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(columnDefinition = "TEXT")
+    private String content;
+    private LocalDateTime createDate;
+    @ManyToOne      //  N:1 의 관계로 Article 클래스와 연결된다. (FK)
+    private Article article;
+
+}
